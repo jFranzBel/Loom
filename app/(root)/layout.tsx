d834e4420e3520b206f01e78f0137eb8f907b99f
jFranzBel <jonnathan.franz.87@outlook.com>
2024-05-29 10:43:14 -0600

    Refactoring and indentantion update

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,11 @@
 import React, { ReactNode } from 'react';
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
-    return (
-        <main>
-            {children}
-        </main>
-    )
+   return (
+      <main>
+         {children}
+      </main>
+   )
 };
 
 export default RootLayout;
--- constants/index.ts
+++ constants/index.ts
@@ -1,29 +1,29 @@
 export const sidebarLinks = [
-   {
-      label: 'Home',
-      imgUrl: '/icons/Home.svg',
-      route: '/',
-   },
-   {
-      label: 'Upcoming',
-      imgUrl: '/icons/upcoming.svg',
-      route: '/upcoming',
-   },
-   {
-      label: 'Previous',
-      imgUrl: '/icons/previous.svg',
-      route: '/previous',
-   },
-   {
-      label: 'Recordings',
-      imgUrl: '/icons/Video.svg',
-      route: '/recordings',
-   },
-   {
-      label: 'Personal Room',
-      imgUrl: '/icons/add-personal.svg',
-      route: '/personal-room',
-   },
+  {
+    label: 'Home',
+    imgUrl: '/icons/Home.svg',
+    route: '/',
+  },
+  {
+    label: 'Upcoming',
+    imgUrl: '/icons/upcoming.svg',
+    route: '/upcoming',
+  },
+  {
+    label: 'Previous',
+    imgUrl: '/icons/previous.svg',
+    route: '/previous',
+  },
+  {
+    label: 'Recordings',
+    imgUrl: '/icons/Video.svg',
+    route: '/recordings',
+  },
+  {
+    label: 'Personal Room',
+    imgUrl: '/icons/add-personal.svg',
+    route: '/personal-room',
+  },
 ];
 
 /* Formated current time and date */
@@ -31,22 +31,22 @@ export const sidebarLinks = [
 const now: Date = new Date();
 
 const dateOptions: Intl.DateTimeFormatOptions = {
-   weekday: 'long',
-   year: 'numeric',
-   month: 'long',
-   day: 'numeric'
+  weekday: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric'
 };
 
 const timeOptions: Intl.DateTimeFormatOptions = {
-   hour: 'numeric',
-   minute: 'numeric',
-   hour12: true,
+  hour: 'numeric',
+  minute: 'numeric',
+  hour12: true,
 };
 
 const formattedTime: string = now.toLocaleTimeString('en-Us', timeOptions);
 const formattedDate: string = now.toLocaleDateString('en-US', dateOptions);
 
 export const timeDate = {
-   time: `${formattedTime}`,
-   date: `${formattedDate}`,
+  time: `${formattedTime}`,
+  date: `${formattedDate}`,
 };
